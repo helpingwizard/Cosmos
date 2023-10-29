75334ce0bb77c853d44d64e37a57330c26fb10dc
Tanish Charthankar <vctanish7@gmail.com>
2023-10-30 00:09:22 +0530

    made signup component partially

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Signup from "./Signup";
+
+
+
+
+
+
+
+function App() {
+  return (
+      <div style={{width:"100vw" ,
+          height:"100vh",
+          backgroundColor: "#04364A" }}>
+          <Signup/>
+          
+  
+  
+      </div>    
+  );
+
+  
+}
+
+
+export default App;
---
+++ src/Signup.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import {Card, Typography, colors} from "@mui/material";
+
+//import axios from "axios";
+
+
+
+
+function Signup() {
+    //const [email, setEmail] = useState("");
+    //const [password, setPassword] = useState("");  
+    
+
+
+    return <div>
+        <div style = {{
+            paddingTop: 150,
+            marginBottom: 10,
+            display: "flex",
+            justifyContent: "center"
+        }}>
+            <Typography variant = "h6" style={{ color: "white" }}>
+            Welcome to Cosmos. Sign up below..
+            </Typography>
+        </div>
+    </div>
+    
+}
+
+
+export default Signup;
